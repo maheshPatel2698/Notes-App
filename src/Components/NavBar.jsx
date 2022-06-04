aaa3b150711dbaceaf60937a6d57ea9df0131a25
maheshPatel2698 <84387647+maheshPatel2698@users.noreply.github.com>
2022-06-04 12:26:53 +0530

    done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -26,45 +26,7 @@ const NavBar = () => {
         arrow.classList.toggle('adcEffect')
     }
     return (
-        <div id='nav' className='nav-bar'>
-            <div className="left">
-                <Link to="/">
-                    Notes App
-                </Link>
-                <BsArrowDownCircleFill id='adc' onClick={handleNavbar} size={25} />
-            </div>
-            <div id='mid' className="mid">
-                <Link to="/notes">
-                    <CgNotes id='cn' size={35} />
-                </Link>
-                <Link to="/addnote">
-                    <FaPlusCircle id='circle' size={35} />
-                </Link>
-            </div>
-            <div id='right' className="right">
-                <div className="imgclass">
-                    <img onClick={handleUsercard} className='img' src={user?.email ? user?.photo : img1} alt="profile" />
-                </div>
-                {!user?.email ? <BiLogIn onClick={handleLogin} size={35} /> : null}
-                <div id='userCard' className="userCard">
-                    <div className="img-class">
-                        <img className='uimg' src={user?.email ? user?.photo : "null"} alt="profile" />
-                    </div>
-                    {user?.email ? <div className='userdetails'>
-                        <span>Name: {user?.name} </span>
-                        <span>Email: {user?.email} </span>
-                        <span>verified: {user?.varifiedEmail ? <TiTick color='green' size={25} /> : ""}</span>
-                        <span>last Login: {user?.lastlogin}  </span>
-                    </div> : ""}
-
-                    <div className="button">
-                        {user?.email ? <BiLogOut size={30} onClick={handleLogOut} /> : ""}
-                    </div>
-                </div>
-                <div className='bulb' >
-                    <FaRegLightbulb onClick={handleDarkMode} size={33} />
-                </div>
-            </div>
+        <div id='nav' className='navbar'>
         </div>
     )
 }
